5875c677be5c7dfada8da00ed4ac5487e3298f67
Charles Williams <crw4147@rit.edu>
2016-04-28 21:13:20 -0400

    Pocket and Ball collisions

---
+++ OpenGLDemoWindow/OpenGLDemoWindow/WilliamsCollisionEngine/shaders/fShader.glsl
@@ -0,0 +1,27 @@
+#version 430
+
+in vec2 fragUV;
+
+uniform sampler2D myTexture;
+
+void main()
+{
+	//if(fragUV.x < 0 && fragUV.y < 0)
+	//{
+	//	gl_FragColor = vec4(-fragUV.x * .6, -fragUV.y * .75, -fragUV.x * .8, fragUV.y * .8);
+	//}
+	//else if(fragUV.x < 0)
+	//{
+	//	gl_FragColor = vec4(-fragUV.x * .6, fragUV.y * .75, -fragUV.x * .8, fragUV.y * .8);
+	//}
+	//else if(fragUV.y < 0)
+	//{
+	//	gl_FragColor = vec4(fragUV.x * .6, -fragUV.y * .75, -fragUV.x * .8, fragUV.y * .8);
+	//}
+	//else
+	//{
+	//	gl_FragColor = vec4(fragUV.x * .6, fragUV.y * .75, -fragUV.x * .8, fragUV.y * .8);
+	//}
+	
+	gl_FragColor = texture(myTexture, fragUV);
+}
